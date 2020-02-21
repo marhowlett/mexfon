d9bcef8e75fb6e121388b77629175b71e1b40c99
Lyngall <37223445+ItsShinyAlex@users.noreply.github.com>
2020-02-20 19:03:22 -0600

    html portabilidad

---
+++ portabilidad.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MEXFON</title>
+<?php include 'headers.php';?>
+			<title>MEXFON - NUESTROS PAQUETES</title>
+
+            
+ 
+
+
+
+</head>
+
+
+
+
+<body>
+
+
+<?php include 'topbar.php';?>
+
+<?php include 'green-menu.php';?>
+
+<section id="portabilidad"  class="banner-mx" >
+
+<div class="contain ">
+    
+    <div class="flex ">
+        
+        <div class="col ">
+            
+            <div class="">
+                
+                    <h1>PORTABILIDAD <br> <span class="smaller">¡Cámbiate con tu número!</span>  </h1>
+                    
+                
+            </div>
+            
+        
+            
+        </div>
+        <div class="col blank"></div>
+        
+    </div>
+    
+</div>
+
+</section>
+
+<section id="tunumero"  class="addoned" >
+
+<div class="contain center">
+
+
+<p>Estás a un paso de ser parte de MEXFON</p>
+  <p class="light">
+      
+      Déjanos apoyarte con la portabilidad de tu número
+      
+  </p>   
+   
+<img src="img/qs/addon.png" alt="addon">
+    
+</div>
+
+</section>
+
+
+<section id="portabilidad-form"  class="" >
+
+<div class="contain formclass">
+
+    
+    <div class="flex">
+        
+        <div class="mastercol col">
+            
+            
+             <div class="container">
+                
+                
+                    <form action="/action_page.php">
+
+                    <input type="text" id="name" name="name" placeholder="Nombre" required>
+                    
+                    <input type="text" id="email" name="email" placeholder="Correo electrónico" required>
+                    
+                    <input type="text" id="tel" name="tel" placeholder="Teléfono" required>
+
+
+                    <div class="center">
+                        
+                        <a href="olvide.php"> </a>
+
+                        <input type="submit" value="CONTÁCTAME" class="btn">
+
+                    </div>
+
+
+                  </form>
+                  
+                  <p> Al dar click aceptas nuestro  <a href=""> <b>aviso de privacidad</b> </a> </p>
+
+                
+            </div>
+            
+            
+        </div>
+        
+        <div class="col imgcol">
+            
+            <img src="img/portabilidad/phone.jpg" alt="smartphone mexfon">
+            
+        </div>
+        
+        
+    </div>
+
+
+</div>
+
+</section>
+
+
+
+
+<?php include 'bot-bar.php';?>
+
+</body>
+</html>
